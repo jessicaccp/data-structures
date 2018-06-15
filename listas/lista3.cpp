536c09faa1adeaffed100d3c3b233f9970c4bfe8
Jessica Cacau <jessicacacau@gmail.com>
2018-06-15 14:49:49 -0300

    questao 2

--- listas/lista3.cpp
+++ listas/lista3.cpp
@@ -169,22 +169,65 @@ class Nodo {
 /* Questao 1: Faca uma funcao que receba duas pilhas numericas ordenadas
 crescentemente a partir do topo. Ela deve retornar uma nova pilha de modo que
 fique ordenada decrescentemente com o maior valor no topo. */
-Pilha* questao1 (Pilha *p1, Pilha *p2) {
+Pilha* questao1(Pilha *p1, Pilha *p2) {
+	// cria uma nova pilha
 	Pilha *p3;
 	p3->criarPilha();
 
+	// enquanto as pilhas 1 e 2 nao estao vazias
 	while ((not p1->isEmpty()) and (not p2->isEmpty())) {
+		// adiciona topo de p2 em p3 se p1 estiver vazia
 		if (p1->isEmpty())
 			p3->push(p2->pop());
+		// adiciona topo de p1 em p3 se p2 estiver vazia
 		else if (p2->isEmpty())
 			p3->push(p1->pop());
+		// adiciona topo de p1 se for menor que o topo de p2
 		else if (p1->top() < p2->top())
 			p3->push(p1->pop());
+		// adiciona topo de p2 se for menor ou igual que o topo de p1
 		else
 			p3->push(p2->pop());
 	}
 	
+	// retorna a nova pilha
 	return p3;
+}
+
+/* Elabore um algoritmo para verificar se um dado item esta presente em uma
+pilha. Em caso positivo, o algoritmo deve fornecer tambem a posicao do item na
+pilha, considerando o topo como posicao 1. A pilha deve permanecer a mesma apos
+a execucao do procedimento. Use somente as funcoes PUSH e POP. */
+int questao2(Pilha *p1, int info) {
+	// cria uma nova pilha
+	Pilha *p2;
+	p2->criarPilha();
+	
+	// seta a posicao, comecando pelo topo
+	int posicao = 1;
+	
+	// checa enquanto a pilha nao estiver vazia
+	while (not p1->isEmpty()) {
+		// se o item procurado nao for o topo da pilha,
+		// remove o topo da pilha 1, o empilha na pilha 2
+		// e atualiza o valor da posicao do nodo de p1
+		if (p1->top() != info) {
+			p2->push(p1->pop());
+			posicao++;
+		}
+		// se o item estiver no topo da pilha 1,
+		// restaura a pilha p1 e retorna a posicao do item na pilha
+		else {
+			while (not p2->isEmpty())
+				p1->push(p2->pop());
+			return posicao;
+		}
+	}
+	
+	// caso nao encontre o item na pilha, restaura a pilha p1 e retorna zero
+	while (not p2->isEmpty())
+		p1->push(p2->pop());
+	return 0;
 }
 
 /* funcao principal */
