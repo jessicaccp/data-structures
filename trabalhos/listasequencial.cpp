92a8ceea15970154264f59fdb8412efe2679a3cd
Camila <barbosacamila97@gmail.com>
2018-07-07 04:47:25 -0300

    fiz comentários, inseri umas funções que faltavam
    falta testar

--- trabalhos/listasequencial.cpp
+++ trabalhos/listasequencial.cpp
@@ -2,14 +2,10 @@
 // Centro de Ciencias e Tecnologia
 // Curso de Ciencia da Computacao
 // Disciplina de Estruturas de Dados 1 2017.2 - Prof. Bruno Lima
-// Aluna: Jessica Cristina Cacau Patricio (1388627)
-
+// Alunas: Jessica Cristina Cacau Patricio (1388627)
+//		  Camila Alves Barbosa (1394330)
 // Listas sequenciais
-// Metodos: criarLista, isEmpty, isFull, getSize, insert (fim), insert (pos),
-//
-
-// Metodos: criarFila, clear, isEmpty, enfileira, desenfileira, size, print,
-// search, concatenar, intercalar, dividir, ordenar
+// Metodos: criarLista, isEmpty, isFull, getSize, insert (fim), insert (pos), insert(ord), remove(index), print.
 
 /*
 public void insert(int element, int pos) - insere um novo elemento em uma determinada posição passada por parâmetro
@@ -52,21 +48,21 @@ class Lista {
 			lista[i] = -1;
 	}
 	
-	// Verifica se a lista esta vazia
+	// Verifica se a lista esta vazia.
 	bool isEmpty() {
 		if (lista[0] == -1)
 			return true;
 		return false;
 	}
 	
-	// Verifica se a lista esta cheia
+	// Verifica se a lista esta cheia.
 	bool isFull() {
 		if (lista[getTamanho() - 1] != -1)
 			return true;
 		return false;
 	}
 	
-	// Retorna o numero de elementos na lista
+	// Retorna o numero de elementos na lista.
 	int getSize() {
 		int cont = 0;
 		for (int i = 0; i < getTamanho(); i++)
@@ -98,153 +94,157 @@ class Lista {
 	}
 	
 	// Insere elemento na posicao desejada
-	void insert(int info, int pos) {
-
+	// Checa se o lugar onde o usuario que inserir e valido, se não for entra no primeiro if.
+	// Se for valido e se a lista nao estiver cheia, entra no primeiro else if.
+	// Se ela estiver cheia, 
+	void insert(int element, int pos) {
+		if (pos>tamanho or pos>getSize()) 
+			cout << "Impossível inserir nessa posição." << endl;
+		else if (pos == tamanho)
+			insert(element);
+		else { 
+			if (isFull())
+				insert(vetor[tamanho-1]);
+			for (int i=getSize()-1; i>=pos; i--)
+				vetor[i+1] = vetor[i];
+			vetor[pos] = element;
+		}
 	}
 	
+	// Funcao de ordenacao para ordenar o vetor.
+	 void ordenar() {
+	 	for (int i=0; i<getSize()-1; i++)
+	 		for (int j=i; j<getSize(); j++)
+	 			if (vetor[i] > vetor[j])
+	 				swap(vetor[i], vetor[j]);
+	 }
 	
-	//public void insertOrd(int element) - insere um novo elemento de forma ordenada, ou seja, ao inserir um novo elemento a lista deve continuar ordenada em ordem crescente
+	// Insere um novo elemento de forma ordenada
+	// Primeiro ordena-se a lista para depois realizar a insercao.
+	void insertOrd(int element) {
+		ordenar();
+		for (int i=0; i<getSize(); i++)
+			if (element<vetor[i])
+				insert(element, i);
+	}
+	
+	// Remove um elemento da posicao index da lista
+	// Primeiro checa-se se na posicao existe um elemento ou se a posicao que sera eliminada existe.
+	// Se ele existir, o elemento sera removido e o tamanho da lista diminuira.
+	bool remove(int index) {
+		if (index >= tamanho or lista[index] == -1)
+			return false;
+		for (int i=index; i<getSize()-1; i++)
+			lista[i] = lista[i+1];
+		lista[getSize()-1] = -1;
+		tamanho--;
+	}
+	
+	// Imprime todos os elementos da lista
+	void print() {
+		if (isEmpty())
+			cout << "Lista vazia." << endl;
+		else {
+			for (int i=0; i<getSize(); i++)
+				cout << lista[i] << " ";
+			cout << endl;
+		}
+	}
+	
 };
-
-// Metodos: criarFila, clear, isEmpty, enfileira, desenfileira, size, print,
-// search, concatenar, intercalar, dividir, ordenar
+// Metodos: criarFila, isEmpty, getSize, insert(info), insert(element, pos), insertOrd(element), remove(index), print.
 int main () {
-	Fila *f = new Fila();
-	f->criarFila();
-	int op = 0, info = 0;
-	
-	Fila *f1 = new Fila();
-	Fila *f2 = new Fila();
-	f1->criarFila();
-	f2->criarFila();
+	Lista *l = new Lista();
+	l->criarLista();
+	info = 0;
 	
 	// Menu
 	while (true) {
 		// Opcoes
 		op = 0;
 		cout	<< endl
-				<< " 1. clear"			<< endl
-				<< " 2. isEmpty"		<< endl
-				<< " 3. enfileira"		<< endl
-				<< " 4. desenfileira"	<< endl
-				<< " 5. size"			<< endl
-				<< " 6. print"			<< endl
-				<< " 7. search"			<< endl
-				<< " 8. concatenar"		<< endl
-				<< " 9. intercalar"		<< endl
-				<< "10. dividir"		<< endl
-				<< "11. ordenar"		<< endl
-				<< "12. sair"			<< endl;
+				<< " 1. isEmpty"						<< endl
+				<< " 2. isFull"							<< endl
+				<< " 3. getSize"						<< endl
+				<< " 4. insercao no final"				<< endl
+				<< " 5. insercao com detalhamentos"		<< endl
+				<< " 6. insercao ordenada"				<< endl
+				<< " 7. remocao de um elemento"			<< endl
+				<< " 8. imprimir todos os elementos"	<< endl
+				<< " 9. sair"							<< endl;
 		
 		// Faz a leitura da opcao
-		while (op < 1 or op > 12) {
+		while (op < 1 or op > 7) {
 			cout << endl << "Opcao: ";
 			cin >> op;
-			if (op < 1 or op > 12)
+			if (op < 1 or op > 7)
 				cout << endl << "Opcao invalida." << endl;
 		}
 		
-		// Realiza o comando de acordo com a opcao
+		// Realiza o comando de acordo com a opcao.
 		switch (op) {
-			// Remove todos os nodos da fila
+			// Verifica se a lista esta vazia.
 			case 1:
-				f->clear();
-				cout << endl << "Fila apagada." << endl;
+				if (l->isEmpty())
+					cout << endl << "Lista vazia." << endl;
+				else
+					cout << endl << "Lista nao vazia." << endl;
 				break;
 			
-			// Verifica se a fila esta vazia
+			// Verifica se a lista esta cheia.
 			case 2:
-				if (f->isEmpty())
-					cout << endl << "Fila vazia." << endl;
+				if (l->isFull())
+					cout << endl << "Lista cheia." << endl;
 				else
-					cout << endl << "Fila nao vazia." << endl;
+					cout << endl << "Lista nao vazia." << endl;
 				break;
 			
-			// Insere valor no fim da fila
+			// Imprime o tamanho da lista.
 			case 3:
-				cout << endl << "Insira o valor do elemento a ser adicionado: ";
-				cin >> info;
-				f->enfileira(info);
-				cout << info << " adicionado na fila." << endl;
-				break;
-				
-			// Remove valor do inicio da fila
+				cout << endl << "Numero de elementos: " << l->getSize() << "." << endl;
+			
+			// Insere o valor no final da lista
 			case 4:
-				if (f->isEmpty())
-					cout << endl << "Fila vazia." << endl;
-				else
-					cout << endl << f->desenfileira() << " removido da fila." << endl;
-				break;
-				
-			// Verifica tamanho da fila
-			case 5:
-				cout << endl << "Tamanho da fila: " << f->size() << "." << endl;
-				break;
-			
-			// Exibe todos os elementos da fila, se existirem
-			case 6:
-				if (f->isEmpty())
-					cout << endl << "Fila vazia." << endl;
-				else {
-					cout << endl << "Elementos da fila: ";
-					f->print();
-				}
-				break;
-				
-			// Busca valor informado na fila
-			case 7:
-				cout << endl << "Insira o valor do elemento a ser procurado: ";
+				ccout << endl << "Insira o valor do elemento a ser adicionado na lista: ";
 				cin >> info;
-				if (f->search(info))
-					cout << info << " existe na fila." << endl;
-				else
-					cout << info << " nao existe na fila." << endl;
+				l->insert(info);
+				cout << info << "Elemento adicionado na lista." << endl;
 				break;
 			
-			// Concatena duas filas recebidas em uma
+			// Insere valor desejado na posicao desejada.
+			case 5:
+				cout << endl << "Insira o elemento que voce quer inserir: ";
+				cin >> element;
+				cout << endl << "Insira a posicao a qual voce quer inserir o elemento: ";
+				cin >> pos;
+				l->insert(element, pos);
+				cout << endl << "Elemento adicionado na lista." << endl;
+				break;
+				
+			// Insere o valor desejdao de uma forma ordenada.
+			case 6:
+				cout << endl << "Insira o elemento que voce quer inserir: ";
+				cin >> element;
+				l->insertOrd(element);
+				cout << endl << "Elemento adicionado na lista." << endl;
+				break;
+				
+			// Remove o elemento desejado da lista.
+			case 7:
+				cout << endl << "Insira a posicao que voce quer remover: ";
+				cin << index;
+				l->remove(index);
+				cout << "Elemento removido." << endl;
+				break;
+			
+			// Exibe todos os elementos da lista, se existirem.
 			case 8:
-				cout << endl << "Menu nao implementado ainda." << endl;
+				cout << endl << "Elementos da fila: ";
+				l->print();
 				break;
-				// concatenar
-				// recebe duas filas e exibe o final
-				
-			// Intercala duas filas recebidas em uma
-			case 9:
-				cout << endl << "Menu nao implementado ainda." << endl;
-				break;
-				// intercalar
-				// recebe duas filas e exibe a final
-				
-			// Divide a fila em duas que sao passadas por parametro
-			case 10:
-				if (f->isEmpty())
-					cout << endl << "Fila vazia." << endl;
-				else {
-					cout << endl << "Fila original: ";
-					f->print();
-					f->dividir(f1, f2);
-					cout << endl << "Filas divididas:" << endl << "F1 - ";
-					f1->print();
-					cout << "F2 - ";
-					f2->print();
-				}
-				break;
-				
-			// Ordena os valores da fila
-			case 11:
-				if (f->isEmpty())
-					cout << endl << "Fila vazia." << endl;
-				else {
-					cout << endl << "Lista desordenada: ";
-					f->print();
-					f->ordenar();
-					cout << endl << "Lista ordenada: ";
-					f->print();
-				}
-				break;
-			
+						
 			// Sai do programa
-			case 12:
+			case 9:
 				exit(0);
 				
 			default:
